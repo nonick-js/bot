d9b6c0bd3f2e8e6ce8a10d1939f1bf8b4b95d87f
nonick-mc <nonick.mc1017@gmail.com>
2022-04-10 14:59:50 +0900

    モジュールの追加
    ・discord-modals
    ・@discordjs/builders
    ・@discordjs/rest
    ・discord-api-types

--- index.js
+++ index.js
@@ -8,3 +8,46 @@ http.createServer(function(req, res) {
 	res.end();
 }).listen(8080);
 */
+
+const fs = require('node:fs');
+const { Client, Collection, Intents, MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
+const { serverName } = require('./config.json');
+const discordModals = require('discord-modals');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+discordModals(client);
+require('dotenv').config();
+
+// ready nouniku!!(定期)
+client.once('ready', () => {
+	console.log(`[DiscordBot-NoNick.js]`+'\u001b[32m'+' DiscordBotが起動しました。'+'\u001b[0m');
+	client.user.setActivity(`${serverName}`);
+});
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+// コマンド処理
+client.on('interactionCreate', async interaction => {
+	if (interaction.isCommand()) {
+		const command = client.commands.get(interaction.commandName);
+		if (!command) return;
+
+		try {
+			await command.execute(interaction,client);
+		} catch (error) {
+			console.error(error);
+			const embed = new MessageEmbed()
+				.setColor('#F61E2')
+				.setDescription('コマンドの実行中にエラーが発生しました、開発者にご連絡ください。')
+			await interaction.reply({embeds: [embed], ephemeral: true});
+		}
+	}
+});
+
+client.login(process.env.BOT_TOKEN);
