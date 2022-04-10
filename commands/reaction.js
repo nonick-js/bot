ffd75fcb11e6ed05055bdfabec53f0c7ac1aefc2
nonick-mc <nonick.mc1017@gmail.com>
2022-04-10 16:30:44 +0900

    reactionコマンド
    modal本体の完成

--- commands/reaction.js
+++ commands/reaction.js
@@ -1,10 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Modal, TextInputComponent, showModal } = require('discord-modals');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('reacitonrole')
 		.setDescription('リアクションロールが可能な埋め込みを送信します'),
-	async execute(interaction) {
-		interaction.reply('Pong!');
+	async execute(interaction,client) {
+		const modaltest = new Modal()
+            .setCustomId('reactionmodal')
+            .setTitle('リアクションロール')
+            .addComponents(
+            new TextInputComponent()
+                .setCustomId('textinput-title')
+                .setLabel('タイトル')
+                .setStyle('SHORT')
+                .setPlaceholder('ここに入力')
+                .setRequired(true),
+            new TextInputComponent()
+                .setCustomId('textinput-description')
+                .setLabel('説明')
+                .setStyle('LONG')
+                .setPlaceholder('<#チャンネルID> <@ユーザーID> <@&ロールID> で埋め込み内でメンションができます。')
+                .setRequired(true)
+            );         
+        showModal(modaltest, {client, interaction});
 	},
 };
