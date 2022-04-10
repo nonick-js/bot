69850660ff9a5d129d55d92cad731c57f8dbe385
nonick-mc <nonick.mc1017@gmail.com>
2022-04-10 14:54:47 +0900

    Repl.itホスティングコードの追加

---
+++ index.js
@@ -0,0 +1,10 @@
+//Repl.itでホスティングをする場合は、このコードを有効化する必要がある
+
+/*
+"use strict";
+const http = require('http');
+http.createServer(function(req, res) {
+	res.write("ready nouniku!!");
+	res.end();
+}).listen(8080);
+*/
