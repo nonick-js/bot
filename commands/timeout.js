67e86e167067f5a735e5948f68b1bc695c6f51eb
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-18 17:57:50 +0900

    例外処理
    28日を超えるタイムアウトをしようとした際にエラーで弾くように追加

--- commands/timeout.js
+++ commands/timeout.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu, IntegrationApplication } = require('discord.js');
 const { userInfo } = require('os');
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,16 +8,21 @@ module.exports = {
 		.setDescription('ユーザーをタイムアウトします。 公式のtimeoutコマンドより柔軟な設定が可能です。')
 		.addUserOption(option0 =>
 			option0.setName('user')
-				.setDescription('タイムアウトするユーザーの名前を入力してください。')	
+				.setDescription('タイムアウトするユーザーを選択してください。')	
 				.setRequired(true)
 		)
-		.addNumberOption(option1 => 
-			option1.setName('duration')
+		.addNumberOption(option1 =>
+			option1.setName('day')
+				.setDescription('タイムアウトする時間を日単位で入力してください。')
+				.setRequired(true)
+		)
+		.addNumberOption(option2 => 
+			option2.setName('minute')
 				.setDescription('タイムアウトする時間を分単位で入力してください。')
 				.setRequired(true)
 		)
-		.addStringOption(option2 =>
-			option2.setName('reason')
+		.addStringOption(option3 =>
+			option3.setName('reason')
 				.setDescription('タイムアウトする理由を入力してください。')
 		),
 	async execute(interaction) {
@@ -29,9 +34,19 @@ module.exports = {
             return;
         }
 		const timeoutUser = interaction.options.getUser('user').id;
-		const timeoutMember = interaction.guild.members.cache.get(timeoutUser)
-		const timeoutDuration = interaction.options.getNumber('duration');
+		const timeoutMember = interaction.guild.members.cache.get(timeoutUser);
+		const timeoutDuration_d = interaction.options.getNumber('day');
+		const timeoutDuration_m = interaction.options.getNumber('minute');
 		const timeoutReason = interaction.options.getString('reason');
+		const timeoutDuration = (timeoutDuration_d * 60 * 1000 * 24) + (timeoutDuration_m * 60 * 1000);
+		console.log(timeoutReason);
+		if (timeoutDuration > 40320000) {
+			const embed = new MessageEmbed()
+				.setDescription('⛔ **28日**を超えるタイムアウトはできません!')
+				.setColor('RED');
+			interaction.reply({embeds: [embed], ephemeral: true});
+			return;
+		}
 		timeoutMember.timeout(timeoutDuration * 60 * 1000);
 		await interaction.reply({content: `${timeoutUser}のタイムアウトに成功しました。`, ephemeral:true });
     }
