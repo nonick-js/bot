28b700cf10f879cfe61ea543ad7d4917def4959b
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-19 01:09:53 +0900

    button moduleの作成

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { Client, Collection, Intents, MessageEmbed, MessageActionRow, MessageButt
 const { Modal, TextInputComponent, showModal } = require('discord-modals');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 const discordModals = require('discord-modals');
-const setting_module = require('./modules/setting');
+const interaction_module = require('./interaction/button');
 discordModals(client);
 require('dotenv').config();
 
@@ -71,48 +71,14 @@ client.on('interactionCreate', async interaction => {
 		}
 	}
 	if (interaction.isButton()) {
-		// 入退室ログ設定
-		if (interaction.customId == 'setting1-enable') {
-			const { welcome, welcomeCh } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
-			if (welcome) {
-				setting_module.change_setting("welcome", false);
-				interaction.reply({content: '入退室ログを**オフ**にしました。', ephemeral: true});
-			} else {
-				if(welcomeCh == null) {interaction.reply({content: '**入退室ログを送信するチャンネルIDが指定されていません。**\nセレクトメニュー→「ログを送信するチャンネルの変更」から設定してください。', ephemeral:true}); return;}
-				setting_module.change_setting("welcome", true);
-				interaction.reply({content: '入退室ログを**オン**にしました。', ephemeral: true});
-			}
-		}
-		if (interaction.customId == 'setting1-restore') {
-			setting_module.restore_welcome();
-			interaction.reply({content: '💥 **設定を初期状態に復元しました。**', ephemeral:true});
-		}
-
-		if (interaction.customId == 'timeoutSetting-logEnable') {
-			const { timeoutLog, timeoutLogCh } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
-			if (timeoutLog) {
-				setting_module.change_setting("timeoutLog", false);
-				interaction.reply({content: 'タイムアウトログを**オフ**にしました。', ephemeral: true});
-			} else {
-				if(timeoutLogCh == null) {interaction.reply({content: '**タイムアウトログを送信するチャンネルIDが指定されていません。**\nセレクトメニュー→「ログを送信するチャンネルの変更」から設定してください。', ephemeral:true}); return;}
-				setting_module.change_setting("timeoutLog", true);
-				interaction.reply({content: 'タイムアウトログを**オン**にしました。', ephemeral: true});
-			}
-		}
-		if (interaction.customId == 'timeoutSetting-dmEnable') {
-			const { timeoutDm, timeoutDmString } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
-			if (timeoutDm) {
-				setting_module.change_setting("timeoutDm", false);
-				interaction.reply({content: 'タイムアウトした人への警告DMを**オフ**にしました。', ephemeral: true});
-			} else {
-				if(timeoutDmString == null) {interaction.reply({content: '**警告DMに送信する内容が指定されていません。**\nセレクトメニュー→「警告DMに送信するメッセージの変更」から設定してください。', ephemeral:true}); return;}
-				setting_module.change_setting("timeoutDm", true);
-				interaction.reply({content: 'タイムアウトした人への警告DMを**オン**にしました。', ephemeral: true});
-			}
-		}
-		if (interaction.customId == 'timeoutSetting-restore') {
-				setting_module.restore_timeout();
-				interaction.reply({content: '💥 **設定を初期状態に復元しました。**', ephemeral:true});
+		try {
+			await interaction_module.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			const embed = new MessageEmbed()
+			.setColor('#F61E2')
+			.setDescription('インタラクションの実行中にエラーが発生しました。開発者にご連絡ください。')
+			await interaction.reply({embeds: [embed], ephemeral: true});
 		}
 	}
 
