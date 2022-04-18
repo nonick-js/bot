06798cf32e1fc1c42d645eeceb584e53f6077183
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-18 22:28:24 +0900

    timeout設定のためのModalの追加

--- index.js
+++ index.js
@@ -201,6 +201,26 @@ client.on('modalSubmit', async (modal) => {
 		setting_module.change_setting("welcomeMessage", string);
 		modal.followUp({content: 'メッセージを以下の通りに編集しました。' + Formatters.codeBlock('markdown', string), ephemeral: true});
 	}
+
+	if (modal.customId == 'timeoutModal1') {
+		await modal.deferReply({ephemeral: true});
+		const string = modal.getTextInputValue('textinput');
+		try {
+			const messageId = modal.guild.channels.cache.find((channel) => channel.name === string).id;
+			setting_module.change_setting("welcomeCh", messageId);
+			modal.followUp({ content: `タイムアウトログを送るチャンネルを<#${messageId}>に設定しました。`, ephemeral: true });
+		} catch (error) {
+			modal.followUp({ content: `**入力した名前のチャンネルが見つかりません!**\n正しいIDにしているか、BOTが見れるチャンネルに設定しているかチェックしてください!`, ephemeral: true });
+		}
+	}
+
+	if (modal.customId == 'timeoutModal2') {
+		await modal.deferReply({ephemeral: true});
+		const string = modal.getTextInputValue('textinput');
+		setting_module.change_setting("welcomeMessage", string);
+		modal.followUp({content: 'メッセージを以下の通りに編集しました。' + Formatters.codeBlock('markdown', string), ephemeral: true});
+	}
+
 })
 
 // BOTにログイン
