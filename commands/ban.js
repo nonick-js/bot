8f8d8161d69b3138ddf4d0cbe0719c1f4b646e30
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-19 23:47:30 +0900

    banコマンドのファイルの追加

--- commands/ban.js
+++ commands/ban.js
@@ -4,7 +4,29 @@ const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu } = req
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ban')
-		.setDescription('メンバーをBANします。'),
+		.setDescription('ユーザーをBANします。')
+		.addSubcommand(subcommand => 
+			subcommand
+				.setName('id')
+				.setDescription('ユーザーをIDでBAN')
+				.addStringOption(option0 =>
+					option0.setName('userid')
+						.setDescription('BAN 対象のユーザーID')	
+						.setRequired(true)
+				)
+				.addNumberOption(option2 =>
+					option2.setName('delete_messages')	
+						.setDescription('最近のメッセージ履歴をどこまで削除するか')
+						.addChoice('削除しない', 0)
+						.addChoice('過去24時間', 1)
+						.addChoice('過去7日', 7)
+						.setRequired(true)
+				)
+				.addStringOption(option3 => 
+					option3.setName('reason')
+						.setDescription('BANする理由')
+				),
+		),
 	async execute(interaction) {
     }
 }
