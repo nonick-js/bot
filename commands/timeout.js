339f5bcf76fc89cd7a2ca863563f88378883c451
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-26 18:51:09 +0900

    timeoutコマンドの例外処理の修正
    #9 の修正

--- commands/timeout.js
+++ commands/timeout.js
@@ -57,60 +57,60 @@ module.exports = {
 
 		if (timeoutDuration > 2419200000) { //28日をこえたら
 			const embed = new MessageEmbed()
-				.setDescription('⛔**28日**を超えるタイムアウトはできません!')
+				.setDescription('⛔ **28日**を超えるタイムアウトはできません!')
 				.setColor('RED');
 			interaction.reply({embeds: [embed], ephemeral: true});
 			return;
 		}
 		
-		if (!interaction.options.getUser('user').moderatable) {
-			if (timeoutUserId == bot_id) {
-				interaction.reply({content: '私をタイムアウトするだと...?',ephemeral: true});
-				return;
-			}
-			const embed = new MessageEmbed()
-				.setDescription(`<@${timeoutUserId}> のタイムアウトに失敗しました。\nBOTより上の権限を持っているか、サーバーの管理者です。`)
-				.setColor('RED')
-			interaction.reply({embeds: [embed], ephemeral: true});
+		if (timeoutUserId == bot_id) {
+			interaction.reply({content: '私をタイムアウトするだと...?',ephemeral: true});
 			return;
 		}
 
-		timeoutMember.timeout(timeoutDuration);
-		interaction.reply(`⛔<@${timeoutUserId}>を` + `**${timeoutDuration_d}日` + `${timeoutDuration_m}分**`+`タイムアウトしました。`);
-		const { timeoutLog, timeoutDm } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
-	
-		if (timeoutLog) {
-			const { timeoutLogCh } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
-			const embed = new MessageEmbed()
-			.setTitle('⛔タイムアウト')
-			.setThumbnail(timeoutAvaterURL)
-			.addFields(
-				{name: '処罰を受けた人', value: `<@${timeoutUserId}>`},
-				{name: 'タイムアウトした理由', value: timeoutReason, inline: true},
-				{name: '担当者', value: `<@${moderateUserId}>`}
-			)
-			.setColor('RED');
-			await interaction.guild.channels.cache.get(timeoutLogCh).send({embeds: [embed]}).catch(error => {
-				console.log(`[DiscordBot-NoNick.js]`+'\u001b[31m'+' [ERROR]'+'\u001b[0m'+`[DiscordBot-NoNick.js]` + `\u001b[31m'+' [ERROR]'+'\u001b[0m'+' 指定したチャンネルにタイムアウトログを送れませんでした。「/setting」で正しい・BOTが送信できるチャンネルIDを送信してください。`);
-			});
-		}
-	
-		if (timeoutDm) {
-			const { timeoutDmString } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
-			const timeoutServerIcon = interaction.guild.iconURL();
-			const embed = new MessageEmbed()
-				.setTitle('⛔タイムアウト')
-				.setDescription(timeoutDmString)
-				.setThumbnail(timeoutServerIcon)
-				.setColor('RED')
-				.addFields(
-					{name: 'タイムアウトされた理由', value: timeoutReason}
-			);
-			timeoutMember.send({embeds: [embed]}).catch(error => {
+		timeoutMember.timeout(timeoutDuration)
+			.then(() => {
+				interaction.reply(`⛔ <@${timeoutUserId}>を` + `**${timeoutDuration_d}日` + `${timeoutDuration_m}分**`+`タイムアウトしました。`);
+				const { timeoutLog, timeoutDm } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+				if (timeoutLog) {
+					const { timeoutLogCh } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+					const embed = new MessageEmbed()
+					.setTitle('⛔タイムアウト')
+					.setThumbnail(timeoutAvaterURL)
+					.addFields(
+						{name: '処罰を受けた人', value: `<@${timeoutUserId}>`},
+						{name: 'タイムアウトした理由', value: timeoutReason, inline: true},
+						{name: '担当者', value: `<@${moderateUserId}>`}
+					)
+					.setColor('RED');
+						interaction.guild.channels.cache.get(timeoutLogCh).send({embeds: [embed]}).catch(error => {
+						console.log(`[DiscordBot-NoNick.js]`+'\u001b[31m'+' [ERROR]'+'\u001b[0m'+`[DiscordBot-NoNick.js]` + `\u001b[31m'+' [ERROR]'+'\u001b[0m'+' 指定したチャンネルにタイムアウトログを送れませんでした。「/setting」で正しい・BOTが送信できるチャンネルIDを送信してください。`);
+					});
+				}
+				if (timeoutDm) {
+					const { timeoutDmString } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+					const timeoutServerIcon = interaction.guild.iconURL();
+					const embed = new MessageEmbed()
+						.setTitle('⛔タイムアウト')
+						.setDescription(timeoutDmString)
+						.setThumbnail(timeoutServerIcon)
+						.setColor('RED')
+						.addFields(
+							{name: 'タイムアウトされた理由', value: timeoutReason}
+					);
+					timeoutMember.send({embeds: [embed]}).catch(error => {
+						const embed = new MessageEmbed()
+							.setDescription('タイムアウトした人への警告DMに失敗しました。\nフレンド以外からのメッセージ受信を拒否しています。')
+							.setColor('RED')
+						interaction.followUp({embeds: [embed], ephemeral: true});
+					});
+				}
+			})
+			.catch(() => {
 				const embed = new MessageEmbed()
-					.setDescription('タイムアウトした人への警告DMに失敗しました。\nフレンド以外からのメッセージ受信を拒否しています。')
-				interaction.followUp({content: 'タイムアウトした人へのDMに失敗しました。', ephemeral: true});
-			});
-		}
+					.setDescription(`<@${timeoutUserId}> のタイムアウトに失敗しました。\nBOTより上の権限を持っているか、サーバーの管理者です。`)
+					.setColor('RED');
+				interaction.reply({embeds: [embed], ephemeral:true});
+			})
 	}
 }
