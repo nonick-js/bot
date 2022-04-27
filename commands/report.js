0e38f8273f2ec2ac33362e09d4a118c7220bcabc
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-27 10:22:54 +0900

    reportコンテキストメニューの基盤の完成

--- commands/report.js
+++ commands/report.js
@@ -1,11 +1,15 @@
+const fs = require('fs');
+const { ApplicationCommandType } = require('discord-api-types/v10');
 const { ContextMenuCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const { Formatters } = require('discord.js');
+// const discordModals = require('discord-modals');
+// discordModals(client);
+
 module.exports = {
 data: new ContextMenuCommandBuilder()
         .setName('テスト')
-        .setType(ApplicationCommandType.User),
+        .setType(ApplicationCommandType.Message),
 	async execute(interaction) {
-                interaction.reply('テスト')
-        }
+        interaction.reply('テスト')
+    }
 }
