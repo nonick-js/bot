872d883b2ee0e29a07a6c71286f1b8d49f60c687
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-27 12:49:54 +0900

    コンテキストメニューの応答用Modalの作成

--- commands/report.js
+++ commands/report.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 const { ApplicationCommandType } = require('discord-api-types/v10');
 const { ContextMenuCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-// const discordModals = require('discord-modals');
-// discordModals(client);
+const { Modal, TextInputComponent, showModal } = require('discord-modals');
 
 module.exports = {
-data: new ContextMenuCommandBuilder()
-        .setName('テスト')
+    data: new ContextMenuCommandBuilder()
+        .setName('サーバー運営に通報')
         .setType(ApplicationCommandType.Message),
-	async execute(interaction) {
-        interaction.reply('テスト')
+    async execute(interaction,client) {
+        const modal = new Modal()
+			.setCustomId('banidModal1')
+			.setTitle('通報')
+			.addComponents(
+			new TextInputComponent()
+				.setCustomId('textinput')
+				.setLabel('このメッセージはサーバールールの何に違反していますか？詳しい情報を教えてください。')
+				.setPlaceholder('できる限り詳しく入力してください。')
+				.setStyle('LONG')
+				.setRequired(true)
+			);
+		showModal(modal, {client, interaction});
     }
 }
