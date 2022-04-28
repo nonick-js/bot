8d68125a5abfe5ac5d37e747e7259f251817dddf
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-28 22:22:41 +0900

    埋め込みの修正

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
-const { guildId } = require('./config.json')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 const discordModals = require('discord-modals');
 discordModals(client);
--- interaction/modal.js
+++ interaction/modal.js
@@ -97,28 +97,30 @@ module.exports = {
 
             const embed = modal.message.embeds?.[0];
             const embedfield = modal.message.embeds?.[0]?.fields;
-            const reportedMessageAuthor = await interaction.guild.members.fetch(embedfield[0].value.replace(/^../g, '').replace(/.$/, ''))
+            const reportedMessageAuthor = await client.users.fetch(embedfield[0].value.replace(/^../g, '').replace(/.$/, ''))
 			const reportedMessageCh = embedfield[1].value;
 
             const reportUser = modal.user;
             const reportReason = modal.getTextInputValue('textinput');
+            console.log(reportedMessageAuthor.avatarURL())
 
             const reportEmbed = new MessageEmbed()
                 .setTitle('⚠ 通報')
-                .setDescription(`通報者: ${reportUser}\n` + Formatters.codeBlock(`${reportReason}`))
+                .setDescription(`通報者: ${reportUser}\n` + Formatters.codeBlock(`通報理由: ` + `${reportReason}`))
                 .setThumbnail(reportedMessageAuthor.avatarURL())
                 .addFields(
                     {name: '投稿者', value: `${reportedMessageAuthor}`, inline:true},
                     {name: '投稿先', value: `${reportedMessageCh}`, inline:true }
                 )
-                .setColor('YELLOW');
+                .setColor('RED');
             if(embedfield[2].value) {
                 reportEmbed.addFields({name: "メッセージ", value: `${embedfield[2].value}`});
             }
-            if(embed.image.url) {
+            if(embed.image) {
                 reportEmbed.setImage(embed.image.url);
             }
 
+            const { reportCh } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
             client.channels.cache.get(reportCh).send({embeds: [reportEmbed]});
             modal.followUp({content: "**報告ありがとうございます!** 通報をサーバー運営に送信しました!", ephemeral:true});
         }
