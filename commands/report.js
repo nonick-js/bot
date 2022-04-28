4f596f0bbf91ce6df9bb73b960dd3a5848444e5c
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-28 13:57:21 +0900

    権限処理+
    ・botやwebhook,システムメッセージを通報する場合にエラーを表示
    ・自分自身を通報した場合にエラーを表示

--- commands/report.js
+++ commands/report.js
@@ -9,10 +9,20 @@ module.exports = {
         .setName('サーバー運営に通報')
         .setType(ApplicationCommandType.Message),
     async execute(interaction,client) {
-		const reportedUser = interaction.targetMessage.member
+		const reportedUser = interaction.targetMessage.author
 		const reportedMember = await interaction.guild.members.fetch(reportedUser);
+		console.log(client.user)
 
-		if (reportedMember.permissions.has("MANAGE_MESSAGES")) {
+		if (reportedMember == interaction.member) {
+			interaction.reply({content: '自分自身を通報って...(困惑)', ephemeral:true});
+			return;
+		} else if (reportedUser.bot || reportedUser.system) {
+			const embed = new MessageEmbed()
+				.setDescription('BOT、Webhook、システムメッセージを通報することはできません!')
+				.setColor('RED')
+			interaction.reply({embeds: [embed], ephemeral:true});
+			return;
+		} else if (reportedMember.permissions.has("MANAGE_MESSAGES") ) {
 			const embed = new MessageEmbed()
 				.setDescription('サーバー運営者を通報することはできません!')
 				.setColor('RED');
