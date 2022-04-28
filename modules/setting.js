1f5cd001f19d303d92830b833626bafff73a5d9e
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-28 22:26:35 +0900

    report機能の設定の修正

--- modules/setting.js
+++ modules/setting.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const welcome_default = { "welcomeCh":null, "welcomeMessage":"まずはルールを見よう!", "welcome":false };
 const timeout_default = { "timeout":true, "timeoutLog":false, "timeoutLogCh": null, "timeoutDm": false, "timeoutDmString":"あなたはサーバーからタイムアウトされました。" };
 const banid_default = { "banid":true, "banidLog":false, "banidLogCh":null }
+const report_default = { "report":true, "reportCh":null, "reportRoleMention": false, "reportRole": null }
 
 exports.restore_welcome = () => {
     const data = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
@@ -28,6 +29,16 @@ exports.restore_banid = () => {
     const data_map = new Map(Object.entries(data));
     // リセット
     Object.entries(banid_default).forEach(([key, value]) => {
+        data_map.set(key, value)
+    });
+    fs.writeFileSync('./config.json',JSON.stringify(Object.fromEntries(data_map),null,2));
+}
+
+exports.restore_report = () => {
+    const data = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+    const data_map = new Map(Object.entries(data));
+    // リセット
+    Object.entries(report_default).forEach(([key, value]) => {
         data_map.set(key, value)
     });
     fs.writeFileSync('./config.json',JSON.stringify(Object.fromEntries(data_map),null,2));
