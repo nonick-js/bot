1fa17daec0193c467b5c490f0d1c7888d3507d0d
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-04-28 14:30:00 +0900

    設定チェック
    ・reportChがnull値になっている場合に警告を表示

--- commands/report.js
+++ commands/report.js
@@ -2,13 +2,30 @@ const fs = require('fs');
 const { ApplicationCommandType } = require('discord-api-types/v10');
 const { ContextMenuCommandBuilder } = require('@discordjs/builders');
 const { Modal, TextInputComponent, showModal } = require('discord-modals');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Formatters } = require('discord.js');
 
 module.exports = {
     data: new ContextMenuCommandBuilder()
         .setName('サーバー運営に通報')
         .setType(ApplicationCommandType.Message),
     async execute(interaction,client) {
+		const { reportCh } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+		if (reportCh == null) {
+			if (interaction.member.permissions.has("MANAGE_GUILD")) {
+				const embed = new MessageEmbed()
+					.setDescription('⚠ **この機能を使用するには追加で設定が必要です。**\n' + Formatters.inlineCode('/setting') + 'で通報機能の設定を開き、受取先を設定してください。')
+					.setColor('#526ff5')
+				interaction.reply({embeds: [embed], ephemeral:true});
+				return;
+			} else {
+				const embed = new MessageEmbed()
+					.setDescription('⚠ **この機能を使用するには追加で設定が必要です。**\nBOTの設定権限を持っている人に連絡してください。')
+					.setColor('#526ff5')
+				interaction.reply({embeds: [embed], ephemeral:true});
+				return;
+			}
+		}
+
 		const reportedUser = interaction.targetMessage.author
 		if (reportedUser.bot || reportedUser.system) {
 			const embed = new MessageEmbed()
@@ -18,8 +35,8 @@ module.exports = {
 			return;
 		}
 
-		const reportedMember = await interaction.guild.members.fetch(reportedUser);
-		if (reportedMember == interaction.member) {
+		const reportedMember = await interaction.guild.members.fetch(interaction.member);
+		if (reportedMember == reportMember) {
 			interaction.reply({content: '自分自身を通報って...(困惑)', ephemeral:true});
 			return;
 		} else if (reportedMember.permissions.has("MANAGE_MESSAGES") ) {
