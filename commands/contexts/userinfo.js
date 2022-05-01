37fd03c0d7857e4551d4a37d41c79aab55d5197e
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-05-01 17:58:06 +0900

    userinfoコマンド処理の追加

--- commands/contexts/userinfo.js
+++ commands/contexts/userinfo.js
@@ -8,6 +8,39 @@ module.exports = {
         .setName('このユーザーの情報')
         .setType(ApplicationCommandType.User),
     async execute(interaction) {
+        const infoUser = interaction.targetUser
+        const infoMember = await interaction.guild.members.fetch(infoUser);
+        console.log(infoMember)
 
+        const Usertag = infoUser.tag;
+        const UserName = infoUser.username;
+        const UserAvater = infoUser.displayAvatarURL();
+        const UserCreateTime = infoUser.createdTimestamp;
+
+        const MemberName = infoMember.nickname;
+        const MemberAvater = infoMember.displayAvatarURL();
+        const MemberJoinTime = infoMember.joinedTimestamp;
+
+        const embed = new discord.MessageEmbed()
+            .addFields(
+                {name: 'アカウント作成日', value: UserCreateTime},
+                {name: 'サーバー参加日', value: MemberJoinTime},
+            )
+
+        if (UserAvater == MemberAvater) {
+            embed.setAuthor({ name: `${UserName}` + `${Usertag}`});
+            embed.setThumbnail(UserAvater);
+        } else {
+            embed.setAuthor({ name: `${UserName}` + `${Usertag}`, iconURL: `${UserAvater}` });
+            embed.setThumbnail(MemberAvater);
+        }
+
+        if (MemberName) {
+            embed.setTitle(MemberName);
+        } else {
+            embed.setTitle(UserName);
+        }
+
+        interaction.reply({embeds: [embed], ephemeral: true});
 	}
 }
