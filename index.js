9f52cd961405f9b0ec06cecf8e61bcb2f28b4bbf
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-05-03 20:54:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const client = new discord.Client({
 });
 discordModals(client);
 require('dotenv').config();
+const { guildId} = require('./config.json')
+
+const interaction_commands = require('./modules/interaction');
+const commands = new interaction_commands('./commands');
 
 // モジュールを取得
-// const interaction_button = require('./interaction/button');
-// const interaction_selectmenu = require('./interaction/selectmenu');
-// const interaction_modal = require('./interaction/modal');
 const interaciton_error = require('./modules/error');
 
 // ready
@@ -27,63 +28,74 @@ client.on('ready',async () => {
         'Plattform': `${process.platform} | ${process.arch}`,
         'Memory': `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB | ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}MB`
     });
+	commands.register(client, guildId);
     client.user.setActivity(`${client.guilds.cache.size} serverで導入中!`);
 });
 
 // コマンド・コンテキストメニューを動的に取得する
-client.commands = new discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-}
+// client.commands = new discord.Collection();
+// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// for (const file of commandFiles) {
+// 	const command = require(`./commands/${file}`);
+// 	client.commands.set(command.data.name, command);
+// }
 
-client.contexts = new discord.Collection();
-const contextsFiles = fs.readdirSync('./contexts').filter(file => file.endsWith('.js'));
-for (const file of contextsFiles) {
-	const context = require(`./contexts/${file}`);
-	client.contexts.set(context.data.name, context);
-}
+// client.contexts = new discord.Collection();
+// const contextsFiles = fs.readdirSync('./contexts').filter(file => file.endsWith('.js'));
+// for (const file of contextsFiles) {
+// 	const context = require(`./contexts/${file}`);
+// 	client.contexts.set(context.data.name, context);
+// }
 
 // コマンド処理
-client.on('interactionCreate', async interaction => {
-	// スラッシュコマンド
-	if (interaction.isCommand()) {
-		const command = client.commands.get(interaction.commandName);
-		if (!command) return;
-		await command.execute(interaction,client).catch(error => {
-			interaciton_error.execute(interaction,error);
-		});
-	}
-	// メッセージコンテキストメニュー
-	if (interaction.isMessageContextMenu()) {
-		const context = client.contexts.get(interaction.commandName);
-		if (!context) return;
-		await context.execute(interaction,client).catch(error => {
-			interaciton_error.interactionError.execute(interaction,error);
-		});
-	}
-	// ユーザーコンテキストメニュー
-	if (interaction.isUserContextMenu()) {
-		const context = client.contexts.get(interaction.commandName);
-		if (!context) return;
-		await context.execute(interaction,client).catch(error => {
-			interaciton_error.interactionError.execute(interaction,error);
-		});
-	}
-	// ボタン
-	if (interaction.isButton()) {
-		console.log(interaction.customId)
-		await interaction_button.execute(interaction,client).catch(error => {
-			interaciton_error.interactionError.execute(interaction,error);
-		});
-	}
-	// セレクトメニュー
-	if (interaction.isSelectMenu()) {
-		await interaction_selectmenu.execute(interaction,client).catch(error => {
-			interaciton_error.interactionError.execute(interaction, error);
-		});
-	}
+// client.on('interactionCreate', async interaction => {
+// 	// スラッシュコマンド
+// 	if (interaction.isCommand()) {
+// 		const command = client.commands.get(interaction.commandName);
+// 		if (!command) return;
+// 		await command.execute(interaction,client).catch(error => {
+// 			interaciton_error.execute(interaction,error);
+// 		});
+// 	}
+// 	// メッセージコンテキストメニュー
+// 	if (interaction.isMessageContextMenu()) {
+// 		const context = client.contexts.get(interaction.commandName);
+// 		if (!context) return;
+// 		await context.execute(interaction,client).catch(error => {
+// 			interaciton_error.interactionError.execute(interaction,error);
+// 		});
+// 	}
+// 	// ユーザーコンテキストメニュー
+// 	if (interaction.isUserContextMenu()) {
+// 		const context = client.contexts.get(interaction.commandName);
+// 		if (!context) return;
+// 		await context.execute(interaction,client).catch(error => {
+// 			interaciton_error.interactionError.execute(interaction,error);
+// 		});
+// 	}
+// 	// ボタン
+// 	if (interaction.isButton()) {
+// 		console.log(interaction.customId)
+// 		await interaction_button.execute(interaction,client).catch(error => {
+// 			interaciton_error.interactionError.execute(interaction,error);
+// 		});
+// 	}
+// 	// セレクトメニュー
+// 	if (interaction.isSelectMenu()) {
+// 		await interaction_selectmenu.execute(interaction,client).catch(error => {
+// 			interaciton_error.interactionError.execute(interaction, error);
+// 		});
+// 	}
+// });
+
+client.on('interactionCreate',async interaction => {
+    const cmd = commands.getCommand(interaction);
+    try {
+        cmd.exec(interaction,client);
+    }
+    catch (err) {
+        interaciton_error.interactionError.execute(interaction,err);
+    }
 });
 
 // modalを受け取った時の処理
