cef4cf4f7d28b01ed141a335d8138b1bd379b413
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-05-05 21:57:38 +0900

    設定コマンドの移行

--- commands/setting.js
+++ commands/setting.js
@@ -17,6 +17,14 @@ module.exports = {
     data: {name: "setting", description: "BOTのコントロールパネル(設定)を開きます", type: 'CHAT_INPUT'},
     /**@type {InteractionCallback} */
     exec: async (interaction) => {
+        if (!interaction.member.permissions.has("MANAGE_GUILD")) {
+            const embed = new discord.MessageEmbed()
+                .setColor('RED')
+                .setDescription('**あなたにはこの動作を実行する権限がありません！**\n必要な権限: サーバー管理');
+            interaction.reply({embeds: [embed], ephemeral: true});
+            return;
+        }
+        
         const embed = new discord.MessageEmbed()
             .setTitle('🛠 NoNICK.js - 設定')
             .setDescription('NoNICK.jsのコントロールパネルへようこそ!\nここではこのBOTの設定を変更することができます!' + discord.Formatters.codeBlock("markdown", "セレクトメニューから閲覧・変更したい設定を選択しよう!"))
@@ -34,12 +42,15 @@ module.exports = {
         );
         const select = new discord.MessageActionRow().addComponents(
             new discord.MessageSelectMenu()
-                .setCustomId('setting-control-')
+                .setCustomId('setting-control-select')
                 .setPlaceholder('ここから選択')
                 .addOptions([
-                    { label: '通報機能', value: 'setting-control-report', emoji: '966588719635267624' },
+                    { label: '入退室ログ', value: 'setting-control-welcomemessage', emoji: '966596708458983484'},
+                    { label: '通報機能', value: 'setting-control-report', emoji: '966596708458983484' },
+                    { label: '/timeout コマンド', value: 'setting-control-timeout', emoji: '966596708484149289'},
+                    { label: '/ban コマンド', value: 'setting-control-ban', emoji: '966596708484149289'}
                 ]),
         );
-        interaction.reply({embeds: [embed], components: [select, button], ephemetal: true});
+        interaction.reply({embeds: [embed], components: [select, button], ephemeral: true});
     }
 }
