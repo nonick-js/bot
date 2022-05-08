f33e2e861f127ac9d946a27674ed8b49cf8d1a60
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-05-08 16:29:18 +0900

    report機能の最適化
    ・不要な変数の削除
    ・コード整理

--- commands/contexts/report.js
+++ commands/contexts/report.js
@@ -23,11 +23,8 @@ module.exports = {
             const embed = new discord.MessageEmbed()
 				.setDescription('⚠ **この機能を使用するには追加で設定が必要です。**\nBOTの設定権限を持っている人に連絡してください。')
 				.setColor('#526ff5')
-			if (interaction.member.permissions.has("MANAGE_GUILD")) {
-				embed.setDescription('⚠ **この機能を使用するには追加で設定が必要です。**\n' + discord.Formatters.inlineCode('/setting') + 'で通報機能の設定を開き、レポートを受け取るチャンネルを設定してください。')
-			}
-            interaction.reply({embeds: [embed], ephemeral:true});
-			return;
+			if (interaction.member.permissions.has("MANAGE_GUILD")) embed.setDescription('⚠ **この機能を使用するには追加で設定が必要です。**\n' + discord.Formatters.inlineCode('/setting') + 'で通報機能の設定を開き、レポートを受け取るチャンネルを設定してください。');
+			return interaction.reply({embeds: [embed], ephemeral:true});
 		}
 
 		const reportedUser = interaction.targetMessage.author
@@ -35,24 +32,19 @@ module.exports = {
 			const embed = new discord.MessageEmbed()
 				.setDescription('BOT、Webhook、システムメッセージを通報することはできません!')
 				.setColor('RED')
-			interaction.reply({embeds: [embed], ephemeral:true});
-			return;
+			return interaction.reply({embeds: [embed], ephemeral:true});
 		}
 
 		const reportedMember = await interaction.guild.members.fetch(reportedUser);
-		if (reportedMember == interaction.member) {
-			interaction.reply({content: '自分自身を通報って...(困惑)', ephemeral:true});
-			return;
-		} else if (reportedMember.permissions.has("MANAGE_MESSAGES") ) {
+		if (reportedMember == interaction.member) return interaction.reply({content: '自分自身を通報って...(困惑)', ephemeral:true});
+		if (reportedMember.permissions.has("MANAGE_MESSAGES") ) {
 			const embed = new discord.MessageEmbed()
 				.setDescription('このコマンドでサーバー運営者を通報することはできません!')
 				.setColor('RED');
-			interaction.reply({embeds: [embed], ephemeral:true});
-			return;
+			return interaction.reply({embeds: [embed], ephemeral:true});
 		}
 
 		const reportedMessage = interaction.targetMessage;
-
 		const embed = new discord.MessageEmbed()
 			.setTitle('⚠ メッセージを通報')
 			.setDescription('このメッセージを通報してもよろしいですか?' + discord.Formatters.codeBlock('markdown','通報はこのサーバーの運営にのみ送信されます。\n無関係なメッセージの通報や通報の連投は処罰を受ける可能性があります。'))
@@ -62,18 +54,6 @@ module.exports = {
 				{name: "投稿者", value: `${reportedUser}`, inline:true},
 				{name: "投稿先", value: `${reportedMessage.channel} [リンク](${reportedMessage.url})`, inline:true}
 			)
-		
-		if(reportedMessage.content) {
-			embed.addFields({name: 'メッセージ', value: `${reportedMessage.content}`})
-		}
-		
-		if(reportedMessage.attachments.first()) {
-			const reportedMessageFile = reportedMessage.attachments.first();
-			if(reportedMessageFile.height && reportedMessageFile.width) {
-				embed.setImage(reportedMessageFile.url)
-			}
-		}
-		
 		const button = new discord.MessageActionRow().addComponents(
 			new discord.MessageButton()
 				.setCustomId('report')
@@ -81,6 +61,12 @@ module.exports = {
 				.setEmoji('969148338597412884')
 				.setStyle('DANGER')
 		)
+		
+		if(reportedMessage.content) embed.addFields({name: 'メッセージ', value: `${reportedMessage.content}`})
+		if(reportedMessage.attachments.first()) {
+			const reportedMessageFile = reportedMessage.attachments.first();
+			if(reportedMessageFile.height && reportedMessageFile.width) embed.setImage(reportedMessageFile.url)
+		}
 		interaction.reply({embeds: [embed], components: [button], ephemeral:true});
     }
 }
--- interaciton/modals.js
+++ interaciton/modals.js
@@ -3,41 +3,30 @@ const discord = require('discord.js');
 
 module.exports = {
     async execute(modal,client) {
-        await modal.deferReply({ephemeral: true});
-
-        const embed = modal.message.embeds?.[0];
-        const embedfield = modal.message.embeds?.[0]?.fields;
-        const reportedMessageAuthor = await client.users.fetch(embedfield[0].value.replace(/^../g, '').replace(/.$/, ''))
-		const reportedMessageCh = embedfield[1].value;
-
-        const reportUser = modal.user;
-        const reportReason = modal.getTextInputValue('textinput');
-
-        const reportEmbed = new discord.MessageEmbed()
-            .setTitle('⚠ 通報')
-            .setDescription(`通報者: ${reportUser}\n` + discord.Formatters.codeBlock(`通報理由: ` + `${reportReason}`))
-            .setThumbnail(reportedMessageAuthor.avatarURL())
-            .addFields(
-                {name: '投稿者', value: `${reportedMessageAuthor}`, inline:true},
-                {name: '投稿先', value: `${reportedMessageCh}`, inline:true }
-            )
-            .setColor('RED');
-
-        if(embedfield[2].value) {
-            reportEmbed.addFields({name: "メッセージ", value: `${embedfield[2].value}`});
+        if (modal.customId == 'reportModal') {
+            const { reportCh, reportRoleMention, reportRole } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+            const embed = modal.message.embeds[0];
+            const reportedMessageAuthor = await client.users.fetch(embed.fields[0].value.replace(/^../g, '').replace(/.$/, ''))
+            const reportedMessageCh = embed.fields[1].value;
+            const reportUser = modal.user;
+            const reportReason = modal.getTextInputValue('textinput');
+            const reportEmbed = new discord.MessageEmbed()
+                .setTitle('⚠ 通報')
+                .setDescription(`通報者: ${reportUser}\n` + discord.Formatters.codeBlock(`${reportReason}`))
+                .setThumbnail(reportedMessageAuthor.avatarURL())
+                .addFields(
+                    {name: '投稿者', value: `${reportedMessageAuthor}`, inline:true},
+                    {name: '投稿先', value: `${reportedMessageCh}`, inline:true }
+                )
+                .setColor('RED');
+            if(embed.fields[2].value) reportEmbed.addFields({name: "メッセージ", value: `${embed.fields[2].value}`});
+            if(embed.image) reportEmbed.setImage(embed.image.url);
+            if (reportRoleMention) {
+                client.channels.cache.get(reportCh).send({content: `<@&${reportRole}>`, embeds: [reportEmbed]});
+            } else {
+                client.channels.cache.get(reportCh).send({embeds: [reportEmbed]});
+            }        
+            modal.update({content: "**報告ありがとうございます!** 通報をサーバー運営に送信しました!", embeds: [], components: [], ephemeral:true});
         }
-            
-        if(embed.image) {
-            reportEmbed.setImage(embed.image.url);
-        }
-
-        const { reportCh, reportRoleMention, reportRole } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
-        if (reportRoleMention) {
-            client.channels.cache.get(reportCh).send({content: `<@&${reportRole}>` ,embeds: [reportEmbed]})
-            modal.followUp({content: "**報告ありがとうございます!** 通報をサーバー運営に送信しました!", ephemeral:true});
-        } else {
-            client.channels.cache.get(reportCh).send({embeds: [reportEmbed]})
-            modal.followUp({content: "**報告ありがとうございます!** 通報をサーバー運営に送信しました!", ephemeral:true});
-        } 
     }
 }
