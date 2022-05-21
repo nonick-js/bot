8b6be6351214dbcf01926e3f6f673dd0183ffbbb
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-05-21 23:52:01 +0900

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
 const discord = require('discord.js');
+const discordmodal = require('discord-modals');
 
 /**
 * @callback InteractionCallback
 * @param {discord.MessageContextMenuInteraction} interaction
-* @param {...any} [args]
+* @param {discord.Client} client
 * @returns {void}
 */
 /**
@@ -15,8 +15,40 @@ const discord = require('discord.js');
 
 module.exports = {
     /**@type {discord.ApplicationCommandData|ContextMenuData} */
-    data: {name: "embed", description},
+    data: {customid: "embed", description: "埋め込みメッセージを送信" ,type: "CHAT_INPUT"},
     /**@type {InteractionCallback} */
     exec: async (interaction, client) => {
+		const modal = new discordmodal.Modal()
+			.setCustomId('modal-embed')
+			.setTitle('埋め込み')
+			.addComponents(
+                new discordmodal.TextInputComponent()
+                    .setCustomId('title')
+                    .setLabel('タイトル')
+                    .setMaxLength(256)
+                    .setStyle('SHORT'),
+                new discordmodal.TextInputComponent()
+                    .setCustomId('description')
+                    .setLabel('説明')
+                    .setPlaceholder('<#チャンネルID> <@ユーザーID> <@&ロールID> で埋め込み内でメンションができます。')
+                    .setMaxLength(4096)
+                    .setStyle('LONG'),
+                new discordmodal.TextInputComponent()
+                    .setCustomId('color')
+                    .setLabel('カラーコード')
+                    .setPlaceholder('#は不要です')
+                    .setMaxLength(6)
+                    .setStyle('SHORT'),
+                new discordmodal.TextInputComponent()
+                    .setCustomId('image')
+                    .setLabel('画像URL')
+                    .setStyle('SHORT'),
+                new discordmodal.TextInputComponent()
+                    .setCustomId('footer')
+                    .setLabel('フッター')
+                    .setMaxLength(100)
+                    .setStyle('SHORT')
+            );
+		discordmodal.showModal(modal, {client, interaction});
     }
 }
