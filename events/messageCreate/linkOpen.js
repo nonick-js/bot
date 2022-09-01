35d41a071caa12c2e9761d17fe2b4ae957efe28a
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-09-02 03:02:08 +0900

    リンク展開機能の修正 (by akki)

--- events/messageCreate/linkOpen.js
+++ events/messageCreate/linkOpen.js
@@ -1,51 +1,18 @@
+// eslint-disable-next-line no-unused-vars
 const discord = require('discord.js');
-const pagination = require('../../modules/pagination');
+const expansion = require('../../modules/expansion');
+
+/**
+ * @callback messageCreateCallback
+ * @param {import('discord.js').Message}
+ */
 
 module.exports = {
-    async execute(client, message, Configs) {
-        const config = await Configs.findOne({ where: { serverId: message.guild.id } });
-        const linkOpen = config.get('linkOpen');
-        if (!linkOpen) return;
+    /** @type {messageCreateCallback} */
+    async execute(message) {
+        const config = await message.db_config.findOne({ where: { serverId: message.guild.id } });
+        if (!config.get('linkOpen')) return;
 
-        const results = [...message.content.matchAll(/https:\/\/(?:.+\.)?discord(?:.+)?.com\/channels\/(?<guildId>\d+)\/(?<channelId>\d+)\/(?<messageId>\d+)/g)];
-        results.forEach(async v => {
-            try {
-                const channel = await client.channels.fetch(v.groups.channelId, { force: true });
-                if (!channel || !channel.isText()) throw new TypeError('Unknown Channel');
-                const msg = await channel.messages.fetch(v.groups.messageId, { force: true });
-                if (!msg) throw new TypeError('Unknown Message');
-                const contentEmbeds = msg.content ? discord.Util.splitMessage(msg.content, { maxLength: 1024, char: '' }).map(content => {
-                    return new discord.MessageEmbed()
-                        .setTitle('メッセージ展開')
-                        .setColor('WHITE')
-                        .setURL(v[0])
-                        .setAuthor({
-                            name: msg.member?.displayName ?? msg.author.username,
-                            iconURL: msg.member?.displayAvatarURL({ dynamic: true }) ?? msg.author.displayAvatarURL({ dynamic: true }),
-                        })
-                        .addField('メッセージの内容', content);
-                }) : [];
-                const attachmentEmbeds = msg.attachments.map(attachment => {
-                    return new discord.MessageEmbed()
-                        .setTitle('メッセージの内容')
-                        .setColor('WHITE')
-                        .setURL(v[0])
-                        .setAuthor({
-                            name: msg.member?.displayName ?? msg.author.username,
-                            iconURL: msg.member?.displayAvatarURL({ dynamic: true }) ?? msg.author.displayAvatarURL({ dynamic: true }),
-                        })
-                        .setImage(attachment.url);
-                });
-                pagination.message(message, [...contentEmbeds, ...attachmentEmbeds, ...msg.embeds], { parse: [] });
-            }
-            catch (err) {
-                console.log(err);
-                const em = new discord.MessageEmbed()
-                    .setTitle('エラー!')
-                    .setColor('RED')
-                    .setDescription(err.message);
-                message.reply({ embeds: [em], allowedMentions: { parse: [] } });
-            }
-        });
+        expansion.urlExpansion(message);
     },
 };
--- modules/pagination.js
+++ modules/pagination.js
@@ -5,13 +5,14 @@ class Pagination {
 	#nextButton
 	#current
 	#senderOnly
+	#sended
 	/**
 	 * @constructor
-	 * @param {discord.MessageEmbed[]} pages
+	 * @param {discord.EmbedBuilder[]} pages ページ
 	 */
 	constructor(...pages) {
 		/**
-		 * @type {discord.MessageEmbed[]}
+		 * @type {discord.EmbedBuilder[]}
 		 */
 		this.#pages = pages ?? [];
 		/**
@@ -19,17 +20,15 @@ class Pagination {
 		 */
 		this.#previousButton = new discord.ButtonBuilder()
 			.setCustomId('pagination:previousButton')
-			.setLabel('Previous')
 			.setEmoji('◀️')
-			.setStyle(discord.ButtonStyle.Danger);
+			.setStyle(discord.ButtonStyle.Secondary);
 		/**
 		 * @type {discord.ButtonBuilder}
 		 */
 		this.#nextButton = new discord.ButtonBuilder()
 			.setCustomId('pagination:nextButton')
-			.setLabel('Next')
 			.setEmoji('▶')
-			.setStyle(discord.ButtonStyle.Success);
+			.setStyle(discord.ButtonStyle.Secondary);
 		/**
 		 * @type {number}
 		 */
@@ -38,81 +37,108 @@ class Pagination {
 		 * @type {boolean}
 		 */
 		this.#senderOnly = true;
+		/**
+		 * @type {boolean}
+		 */
+		this.#sended = false;
+	}
+
+	#sendedTest() {
+		if(this.#sended) throw new Error('This message sended!');
 	}
 
 	/**
+	 * メッセージを送った本人のみ使えるようにするか
 	 * @param {boolean} only
 	 */
 	useSenderOnly(only) {
+		this.#sendedTest();
 		this.#senderOnly = only;
 		return this;
 	}
 
 	/**
+	 * 戻るボタンのラベル
 	 * @param {string} label
 	 */
 	setPreviousButtonSetLabel(label) {
+		this.#sendedTest();
 		this.#previousButton.setLabel(label);
 		return this;
 	}
 
 	/**
+	 * 戻るボタンの絵文字
 	 * @param {discord.EmojiIdentifierResolvable} emoji
 	 */
 	setPreviousButtonSetEmoji(emoji) {
+		this.#sendedTest();
 		this.#previousButton.setEmoji(emoji);
 		return this;
 	}
 
 	/**
+	 * 戻るボタンのスタイル
 	 * @param {discord.ButtonStyle} style
 	 */
 	setPreviousButtonSetStyle(style) {
+		this.#sendedTest();
 		this.#previousButton.setStyle(style);
 		return this;
 	}
 
 	/**
+	 * 進むボタンのラベル
 	 * @param {string} label
 	 */
 	setNextButtonSetLabel(label) {
+		this.#sendedTest();
 		this.#nextButton.setLabel(label);
 		return this;
 	}
 
 	/**
+	 * 進むボタンの絵文字
 	 * @param {discord.EmojiIdentifierResolvable} emoji
 	 */
 	setNextButtonSetEmoji(emoji) {
+		this.#sendedTest();
 		this.#nextButton.setEmoji(emoji);
 		return this;
 	}
 
 	/**
+	 * 進むボタンのスタイル
 	 * @param {discord.ButtonStyle} style
 	 */
 	setNextButtonSetStyle(style) {
+		this.#sendedTest();
 		this.#nextButton.setStyle(style);
 		return this;
 	}
 
 	/**
-	 * @param {discord.MessageEmbed} page
+	 * ページを追加する
+	 * @param {discord.EmbedBuilder} page ページ
 	 */
 	addPage(page) {
+		this.#sendedTest();
 		this.#pages.push(page);
 		return this;
 	}
 
 	/**
-	 * @param {discord.MessageEmbed[]} pages
+	 * 複数のページを追加する
+	 * @param {discord.EmbedBuilder[]} pages ページ
 	 */
 	addPages(...pages) {
+		this.#sendedTest();
 		this.#pages.push(...pages);
 		return this;
 	}
 
 	/**
+	 * 送信する
 	 * @param {discord.DMChannel | discord.PartialDMChannel | discord.NewsChannel | discord.TextChannel | discord.ThreadChannel | discord.VoiceChannel} channel
 	 * @param {discord.MessageOptions} options
 	 * @param {discord.Message?} message
@@ -125,12 +151,14 @@ class Pagination {
 			embeds: [...(options.embeds || []), currentEmbed.setFooter({ ...currentEmbed.footer, text: `${currentEmbed.footer?.text || ''}Page ${this.#current + 1} / ${this.#pages.length}` })],
 			components: [...(options.components || []), new discord.ActionRowBuilder().addComponents(this.#previousButton, this.#nextButton)]
 		});
+		this.#sended = true;
 		const filter = (i) => i.customId === 'pagination:previousButton' || i.customId === 'pagination:nextButton';
 		const collector = msg.createMessageComponentCollector({
 			filter, time: 600_000
 		});
 		collector.on('collect', async i => {
-			if(this.#senderOnly && message && !i.user.equals(message.author)) return;
+			if(!message) return
+			if(this.#senderOnly && !i.user.equals(message.author)) return;
 			if(i.customId === 'pagination:previousButton') {
 				this.#current = (this.#pages.length + --this.#current) % this.#pages.length;
 			}
@@ -140,14 +168,14 @@ class Pagination {
 			const embed = this.#pages[this.#current];
 			await i.deferUpdate();
 			await i.editReply({
-				embeds: [embed.setFooter({ ...embed.footer, text: `${embed.footer?.text || ''}Page ${this.#current + 1} / ${this.#pages.length}` })]
+				embeds: [embed.setFooter({ ...embed.footer, text: `${embed.footer?.text || ''}Page ${this.current + 1} / ${this.pages.length}` })]
 			});
 			collector.resetTimer();
 		});
-		collector.on('end',() => {
+		collector.on('end', () => {
 			if(msg) {
 				msg.edit({
-					components: msg.components.map(row => row.components.map(button => filter(button) ? button.setDisabled(true) : button))
+					components: msg.components.map(row => row.components.map(button => filter(button) ? discord.ButtonBuilder.from(button).setDisabled(true) : button))
 				});
 			}
 		});
@@ -158,7 +186,7 @@ class Pagination {
 	 * @param {discord.MessageOptions} options
 	 */
 	async replyMessage(message, options = {}) {
-		options.reply.messageReference = options.reply?.messageReference ?? message;
+		options.reply = { messageReference: options.reply?.messageReference ?? message, failIfNotExists: options.reply?.failIfNotExists}
 		await this.sendMessage(message.channel, options, message);
 	}
 
@@ -174,6 +202,7 @@ class Pagination {
 			embeds: [...(options.embeds || []), currentEmbed.setFooter({ ...currentEmbed.footer, text: `${currentEmbed.footer?.text || ''}Page ${this.#current + 1} / ${this.#pages.length}` })],
 			components: [...(options.components || []), new discord.ActionRowBuilder().addComponents(this.#previousButton, this.#nextButton)]
 		});
+		this.#sended = true;
 		const filter = (i) => i.customId === 'pagination:previousButton' || i.customId === 'pagination:nextButton';
 		const collector = msg.createMessageComponentCollector({
 			filter, time: 600_000
@@ -186,21 +215,13 @@ class Pagination {
 			if(i.customId === 'pagination:nextButton') {
 				this.#current = ++this.#current % this.#pages.length;
 			}
-			const embed = this.#pages[this.#current]
-			await i.deferUpdate();
-			await i.editReply({
+			const embed = this.#pages[this.#current];
+			i.update({
 				embeds: [embed.setFooter({ ...embed.footer, text: `${embed.footer?.text || ''}Page ${this.#current + 1} / ${this.#pages.length}` })]
 			});
 			collector.resetTimer();
 		});
-		collector.on('end',() => {
-			if(msg) {
-				msg.edit({
-					components: msg.components.map(row => row.components.map(button => filter(button) ? button.setDisabled(true) : button))
-				});
-			}
-		});
 	}
 }
 
-module.exports = { Pagination };
+module.exports = Pagination;
