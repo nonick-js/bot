22d7ff04e1edaf662717b7b1a8b8bd9cc8080a7c
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-09-05 18:42:28 +0900

    ユーザー情報閲覧コマンドのリファクタリング

--- commands/contexts/userinfo.js
+++ commands/contexts/userinfo.js
@@ -12,14 +12,6 @@ const flagEmoji = {
     VerifiedDeveloper: '966753508705583174',
     CertifiedModerator:'959536411894243378',
 };
-const removeFlag = [
-    'Quarantined',
-    'BotHTTPInteractions',
-    'Quarantined',
-    'Spammer',
-    'TeamPseudoUser',
-    'VerifiedBot',
-];
 
 /** @type {import('@djs-tools/interactions').UserRegister} */
 const ping_command = {
@@ -29,12 +21,10 @@ const ping_command = {
         type: 'USER',
     },
     exec: async (interaction) => {
-        await interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply();
 
         const createTime = discord.time(Math.floor(interaction.targetUser.createdTimestamp / 1000), 'D');
-        const viewFlags = interaction.targetUser.flags.remove(removeFlag);
-        let flags = '';
-        for (let i = 0; i < viewFlags.toArray().length; i++) flags += discord.formatEmoji(flagEmoji[interaction.targetUser.flags.toArray()[i]]);
+        const flags = interaction.targetUser.flags.toArray().filter(v => flagEmoji[v]).map(v => discord.formatEmoji(flagEmoji[v])).join('');
 
         if (!interaction.targetMember) {
             const embed = new discord.EmbedBuilder()
@@ -47,37 +37,35 @@ const ping_command = {
                 ].join('\n'))
                 .addFields(
                     { name: 'アカウント作成日', value: createTime, inline: true },
-                    { name: 'フラッグ', value: flags ? flags : 'なし', inline: true },
+                    { name: 'フラッグ', value: flags || 'なし', inline: true },
                 );
             return interaction.followUp({ embeds: [embed], ephemeral: true });
         }
 
-        const nickName = interaction.targetMember.nickname ?? '__なし__';
+        const nickName = interaction.targetMember.nickname;
         const joinTime = discord.time(Math.floor(interaction.targetMember.joinedTimestamp / 1000), 'D');
         const boostTime = Math.floor(interaction.targetMember.premiumSinceTimestamp / 1000);
-        const roleCollection = interaction.targetMember.roles.cache.filter(role => role.name !== '@everyone').sort((before, after) => {
+        const roles = interaction.targetMember.roles.cache.filter(role => role.name !== '@everyone').sort((before, after) => {
             if (before.position > after.position) return -1;
             return 1;
-        });
-        const roles = roleCollection.size ? roleCollection.map(role => role.toString()).join(' ') : '__なし__';
+        })?.map(role => role.toString())?.join(' ');
 
         const embed = new discord.EmbedBuilder()
             .setThumbnail(interaction.targetMember.displayAvatarURL())
             .setAuthor({ name: interaction.targetUser.tag })
             .setDescription([
-                `${discord.formatEmoji('1005688190931320922')} ニックネーム: **${nickName}**`,
+                `${discord.formatEmoji('1005688190931320922')} ニックネーム: **${nickName || 'なし'}**`,
                 `${discord.formatEmoji('1005688192818761748')} ユーザーID: \`${interaction.targetId}\``,
             ].join('\n'))
-            .setColor(interaction.targetMember.roles.highest.color)
-            .addFields(
+            .setColor(interaction.targetMember.roles.highest.color || 'White')
+            .setFields(
                 { name: 'アカウント作成日', value: createTime, inline:true },
                 { name: 'サーバー参加日', value: joinTime, inline:true },
-                { name: 'フラッグ', value: flags ? flags : 'なし', inline: true },
-                { name: 'ロール', value: roles },
+                { name: 'フラッグ', value: flags || 'なし', inline: true },
+                { name: 'ロール', value: roles || 'なし' },
             );
 
         if (boostTime) embed.addFields({ name: `${discord.formatEmoji('896591259886567434')} SERVER BOOST`, value: `ブーストを開始した日: ${discord.time(boostTime, 'D')} (${discord.time(boostTime, 'R')})` });
-        if (!embed.data.color) embed.setColor('White');
         if (interaction.targetUser.displayAvatarURL() !== interaction.targetMember.displayAvatarURL()) {
             embed.setAuthor({ name: interaction.targetUser.tag, iconURL: interaction.targetUser.displayAvatarURL() });
             embed.setThumbnail(interaction.targetMember.displayAvatarURL());
