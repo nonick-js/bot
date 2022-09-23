ecea9acbfe7ba522633520c83dcecc557acd32c2
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2022-09-24 00:34:10 +0900

    add: mongoDBの接続

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js');
 const cron = require('node-cron');
-const Sequelize = require('sequelize');
+const mongoose = require('mongoose');
 const { DiscordInteractions } = require('@djs-tools/interactions');
 const { guildId, guildCommand, blackList, statusMessage } = require('../../config.json');
 require('dotenv').config();
@@ -17,37 +17,15 @@ const client = new discord.Client({
     partials: [ discord.Partials.Channel, discord.Partials.GuildMember, discord.Partials.Message, discord.Partials.User ],
 });
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     logging: false,
-//     dialect: 'mysql',
-//     dialectOptions: {
-//         ssl:'Amazon RDS',
-//     },
-// });
-
-const sequelize = new Sequelize({
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    storage: 'models/.config.sqlite',
-});
-
-const basicModel = require('../models/basic')(sequelize);
-const welcomeMModel = require('../models/welcomeM')(sequelize);
-const logModel = require('../models/log')(sequelize);
-const verificationModel = require('../models/verification')(sequelize);
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDBへの接続が完了しました'));
 
 const interactions = new DiscordInteractions(client);
 interactions.loadInteractions('./commands');
 
 client.once('ready', () => {
-    basicModel.sync({ alter: true });
-    welcomeMModel.sync({ alter: true });
-    logModel.sync({ alter: true });
-    verificationModel.sync({ alter: true });
-
     console.log(`[${new Date().toLocaleTimeString('ja-JP')}][INFO]ready!`);
     console.table({
         'Bot User': client.user.tag,
@@ -64,25 +42,24 @@ client.once('ready', () => {
     else interactions.registerCommands();
 
     cron.schedule('0 * * * *', date => {
-        client.sequelize = sequelize;
         require('../cron/verificationChange/index').execute(client, date);
     });
 });
 
-client.on('guildCreate', () => client.user.setActivity({ name: `${statusMessage} | ${client.guilds.cache.size} server`, type: discord.ActivityType.Competing }));
-client.on('guildDelete', guild => {
-    client.user.setActivity({ name: `${statusMessage} | ${client.guilds.cache.size} server`, type: discord.ActivityType.Competing });
-    basicModel.destroy({ where: { serverId: guild.id } });
-    logModel.destroy({ where: { serverId: guild.id } });
-    verificationModel.destroy({ where: { serverId: guild.id } });
-});
+// client.on('guildCreate', () => client.user.setActivity({ name: `${statusMessage} | ${client.guilds.cache.size} server`, type: discord.ActivityType.Competing }));
+// client.on('guildDelete', guild => {
+//     client.user.setActivity({ name: `${statusMessage} | ${client.guilds.cache.size} server`, type: discord.ActivityType.Competing });
+//     basicModel.destroy({ where: { serverId: guild.id } });
+//     logModel.destroy({ where: { serverId: guild.id } });
+//     verificationModel.destroy({ where: { serverId: guild.id } });
+// });
 
-client.on('guildBanAdd', ban => moduleExecute(require('../events/guildBanAdd/index'), ban));
-client.on('guildBanRemove', member => moduleExecute(require('../events/guildBanRemove/index'), member));
-client.on('guildMemberAdd', member => moduleExecute(require('../events/guildMemberAdd/index'), member));
-client.on('guildMemberRemove', member => moduleExecute(require('../events/guildMemberRemove/index'), member));
-client.on('guildMemberUpdate', (oldMember, newMember) => moduleExecute(require('../events/guildMemberUpdate/index'), oldMember, newMember));
-client.on('messageCreate', message => moduleExecute(require('../events/messageCreate/index'), message));
+// client.on('guildBanAdd', ban => moduleExecute(require('../events/guildBanAdd/index'), ban));
+// client.on('guildBanRemove', member => moduleExecute(require('../events/guildBanRemove/index'), member));
+// client.on('guildMemberAdd', member => moduleExecute(require('../events/guildMemberAdd/index'), member));
+// client.on('guildMemberRemove', member => moduleExecute(require('../events/guildMemberRemove/index'), member));
+// client.on('guildMemberUpdate', (oldMember, newMember) => moduleExecute(require('../events/guildMemberUpdate/index'), oldMember, newMember));
+// client.on('messageCreate', message => moduleExecute(require('../events/messageCreate/index'), message));
 
 client.on('interactionCreate', async interaction => {
     if (blackList.guilds.includes(interaction.guild?.id) || blackList.users.includes(interaction.guild?.ownerId)) {
@@ -91,19 +68,13 @@ client.on('interactionCreate', async interaction => {
             .setColor('Red');
         return interaction.reply({ embeds: [embed], ephemeral: true });
     }
-    if (interaction.guild) {
-        await basicModel.findOrCreate({ where: { serverId: interaction.guildId } });
-        await welcomeMModel.findOrCreate({ where: { serverId: interaction.guildId } });
-        await logModel.findOrCreate({ where: { serverId: interaction.guildId } });
-        await verificationModel.findOrCreate({ where: { serverId: interaction.guildId } });
-        interaction.sequelize = sequelize;
-    }
+    if (interaction.guild) {}
     interactions.run(interaction).catch(err => {if (err.code !== 0) console.warn(err);});
 });
 
 async function moduleExecute(module, param, param2) {
     if (blackList.guilds.includes(param.guild?.id) || blackList.users.includes(param.guild?.ownerId)) return;
-    if (param.guild) param.sequelize = sequelize;
+    if (param.guild) {}
     module.execute(param, param2);
 }
 
