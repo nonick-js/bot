66831b5283ddad4f438966e88c4bec5552d5a386
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-01-05 22:31:10 +0900

    Update _permissionCheck.js

--- interactions/commands/admin/_permissionCheck.js
+++ interactions/commands/admin/_permissionCheck.js
@@ -1,14 +1,14 @@
 const { EmbedBuilder, Colors } = require('discord.js');
 const { admin } = require('../../../config.json');
 
+const embed = new EmbedBuilder()
+  .setDescription('`❌` あなたにはこのコマンドを実行する権限がありません')
+  .setColor(Colors.Red);
+
 /** @param {import('discord.js').BaseInteraction} interaction */
 const CheckPermission = (interaction) => {
-  const embed = new EmbedBuilder()
-    .setDescription('`❌` あなたにはこのコマンドを実行する権限がありません。')
-    .setColor(Colors.Red);
-
   if (!admin.users.includes(interaction.user.id)) return embed;
-  return null;
+  return undefined;
 };
 
 module.exports = CheckPermission;
