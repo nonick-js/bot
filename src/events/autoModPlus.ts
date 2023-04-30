c090745500f3d10c81266a48214312ffacfe8bbf
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-04-30 16:32:18 +0900

    メッセージ編集時にAutoModPlusのフィルターを適用するように変更 ( #106 )

--- src/events/autoModPlus.ts
+++ src/events/autoModPlus.ts
@@ -16,31 +16,38 @@ const shortUrlDomain = [
 	'00m.in',
 ];
 
-const autoModPlus = new DiscordEventBuilder({
+const autoModPlusOnMessageCreate = new DiscordEventBuilder({
 	type: Events.MessageCreate,
-	execute: async (message) => {
-		if (!message.inGuild() || !message.member || message.author.bot || message.author.system || message.member.permissions.has(PermissionFlagsBits.ManageGuild)) return;
-		const setting = await getServerSetting(message.guildId, 'autoMod');
-
-		if (
-			!setting?.enable ||
-			setting.ignore.channels.includes(message.channelId) ||
-			message.member.roles.cache.some(role => setting.ignore.roles.includes(role.id))
-		) return;
-
-		const logCh = setting.log.enable && setting.log.channel ? await message.guild.channels.fetch(setting.log.channel) : null;
-
-		if (setting.filter.inviteUrl) {
-			const invites = await message.guild.invites.fetch();
-			if (new RegExp(`(https?:\\/\\/)?(.*\\.)?discord(app)?\\.(com\\/invite|gg)\\/(?!${invites.map(invite => invite.code).join('|')})`).test(message.content)) deleteMessage(message, logCh, 'サーバー招待リンク');
-		}
-		if (setting.filter.token && (
-			/mfa\.[a-z0-9_-]{20,}/i.test(message.content) ||
-			/[a-z0-9_-]{23,28}\.[a-z0-9_-]{6,7}\.[a-z0-9_-]{27}/i.test(message.content)
-		)) deleteMessage(message, logCh, 'Discordアカウントのトークン');
-		if (setting.filter.shortUrl && shortUrlDomain.some(domain => message.content.includes(domain))) deleteMessage(message, logCh, '短縮URL');
-	},
+	execute: async (message) => checkMessage(message),
 });
+
+const autoModPlusOnMessageEdit = new DiscordEventBuilder({
+	type: Events.MessageUpdate,
+	execute: async (beforeMsg, afterMsg) => checkMessage(afterMsg.partial ? await afterMsg.fetch() : afterMsg),
+});
+
+async function checkMessage(message: Message<boolean>) {
+	if (!message.inGuild() || !message.member || message.author.bot || message.author.system || message.member.permissions.has(PermissionFlagsBits.ManageGuild)) return;
+	const setting = await getServerSetting(message.guildId, 'autoMod');
+
+	if (
+		!setting?.enable ||
+		setting.ignore.channels.includes(message.channelId) ||
+		message.member.roles.cache.some(role => setting.ignore.roles.includes(role.id))
+	) return;
+
+	const logCh = setting.log.enable && setting.log.channel ? await message.guild.channels.fetch(setting.log.channel) : null;
+
+	if (setting.filter.inviteUrl) {
+		const invites = await message.guild.invites.fetch();
+		if (new RegExp(`(https?:\\/\\/)?(.*\\.)?discord(app)?\\.(com\\/invite|gg)\\/(?!${invites.map(invite => invite.code).join('|')})`).test(message.content)) deleteMessage(message, logCh, 'サーバー招待リンク');
+	}
+	if (setting.filter.token && (
+		/mfa\.[a-z0-9_-]{20,}/i.test(message.content) ||
+		/[a-z0-9_-]{23,28}\.[a-z0-9_-]{6,7}\.[a-z0-9_-]{27}/i.test(message.content)
+	)) deleteMessage(message, logCh, 'Discordアカウントのトークン');
+	if (setting.filter.shortUrl && shortUrlDomain.some(domain => message.content.includes(domain))) deleteMessage(message, logCh, '短縮URL');
+}
 
 function deleteMessage(message: Message<true>, channel: GuildBasedChannel | null, rule?: string) {
 	message.delete().then(() => {
@@ -52,7 +59,7 @@ function deleteMessage(message: Message<true>, channel: GuildBasedChannel | null
 						.setDescription([
 							`${formatEmoji(Emojis.Gray.channel)} **チャンネル:** ${message.channel} [\`${message.channel.name}\`]`,
 							`${formatEmoji(Emojis.Gray.member)} **送信者:** ${message.author} [\`${message.author.tag}\`]`,
-							`${formatEmoji(Emojis.Gray.schedule)} **送信時刻:** ${time(message.createdAt)}`,
+							`${formatEmoji(Emojis.Gray.schedule)} ${message.editedAt ? `**編集時刻:** ${time(message.createdAt)}` : `**送信時刻:** ${time(message.createdAt)}`}`,
 						].join('\n'))
 						.setColor(resolveColor('#2b2d31'))
 						.setThumbnail(message.author.displayAvatarURL())
@@ -63,4 +70,4 @@ function deleteMessage(message: Message<true>, channel: GuildBasedChannel | null
 	});
 }
 
-module.exports = [autoModPlus];
+module.exports = [autoModPlusOnMessageCreate, autoModPlusOnMessageEdit];
