a4d571b83515afe5a23d396fe4b306f82fc396d3
akki0256 <aki256@outlook.com>
2023-05-09 18:27:08 +0900

    no message

--- src/events/log/delete.ts
+++ src/events/log/delete.ts
@@ -1,4 +1,4 @@
-import { AttachmentBuilder, AuditLogEvent, Collection, Colors, EmbedBuilder, Events, GuildAuditLogsEntry, GuildBasedChannel, Message, User } from 'discord.js';
+import { AttachmentBuilder, AuditLogEvent, Collection, Colors, EmbedBuilder, Events, GuildAuditLogsEntry, Message, MessageFlags } from 'discord.js';
 import { Fields } from '../../module/constant';
 import { DiscordEventBuilder } from '../../module/events';
 import { isBlocked } from '../../module/functions';
@@ -8,51 +8,51 @@ import admZip from 'adm-zip';
 
 const auditLogs = new Collection<string, GuildAuditLogsEntry<AuditLogEvent.MessageDelete> | undefined>();
 
-const selfDeleteLog = new DiscordEventBuilder({
+const title = '`💬` メッセージ削除';
+
+const deleteLog = new DiscordEventBuilder({
 	type: Events.MessageDelete,
 	execute: async (message) => {
 		if (!message.inGuild()) return;
 		if (isBlocked(message.guild)) return;
+		if (message.client.user.equals(message.author) && message.embeds?.[0].title === title) return;
+		if (message.flags.has(MessageFlags.Ephemeral)) return;
+
 		const setting = await getServerSetting(message.guildId, 'log');
 		if (!setting?.delete.enable || !setting.delete.channel) return;
-		const logCh = await message.guild.channels.fetch(setting.delete.channel).catch(() => undefined);
-		const log = await getAuditLog(message);
-		sendDeleteLog(message, logCh, log?.executor);
+		const logCh = await message.guild.channels.fetch(setting.delete.channel).catch(() => { });
+		if (!logCh?.isTextBased()) return;
+		const { executor } = await getAuditLog(message) ?? {};
+		const beforeMessage = (await message.channel.messages.fetch({ before: message.id, limit: 1 })).first();
+
+		const embed = new EmbedBuilder()
+			.setTitle(title)
+			.setURL(beforeMessage?.url ?? null)
+			.setDescription(Fields.multiLine(
+				Fields.channelName(message.channel),
+				Fields.memberTag(message.author, { text: '送信者' }),
+				Fields.memberTag(executor ?? '送信者自身', { text: '削除者' }),
+				Fields.schedule(message.createdAt, { text: '送信時刻' }),
+			))
+			.setColor(Colors.White)
+			.setThumbnail(message.author?.avatarURL() ?? null)
+			.setFields({ name: 'メッセージ', value: message.content || 'なし' });
+
+		if (message.attachments.size) {
+			const zip = new admZip();
+			for await (const attachment of message.attachments.values()) {
+				const res = await axios.get(attachment.url, { responseType: 'arraybuffer' }).catch(() => null);
+				if (!res) continue;
+				zip.addFile(attachment.name, res.data);
+			}
+			const attachment = new AttachmentBuilder(zip.toBuffer(), { name: 'attachments.zip' });
+			logCh.send({ embeds: [embed], files: [attachment] });
+		}
+		else {
+			logCh.send({ embeds: [embed] });
+		}
 	},
 });
-
-async function sendDeleteLog(message: Message<true>, channel?: GuildBasedChannel | null, executer?: User | null) {
-	if (!message.channel || !message.author) return;
-	if (!channel?.isTextBased()) return;
-	const beforeMessage = (await message.channel.messages.fetch({ before: message.id, limit: 1 })).first();
-
-	const embed = new EmbedBuilder()
-		.setTitle('`💬` メッセージ削除')
-		.setURL(beforeMessage?.url ?? null)
-		.setDescription(Fields.multiLine(
-			Fields.channelName(message.channel),
-			Fields.memberTag(message.author, { text: '送信者' }),
-			Fields.memberTag(executer ?? '送信者自身', { text: '削除者' }),
-			Fields.schedule(message.createdAt, { text: '送信時刻' }),
-		))
-		.setColor(Colors.White)
-		.setThumbnail(message.author?.avatarURL() ?? null)
-		.setFields({ name: 'メッセージ', value: message.content || 'なし' });
-
-	if (message.attachments.size) {
-		const zip = new admZip();
-		for await (const attachment of message.attachments.values()) {
-			const res = await axios.get(attachment.url, { responseType: 'arraybuffer' }).catch(() => null);
-			if (!res) continue;
-			zip.addFile(attachment.name, res.data);
-		}
-		const attachment = new AttachmentBuilder(zip.toBuffer(), { name: 'attachments.zip' });
-		channel.send({ embeds: [embed], files: [attachment] });
-	}
-	else {
-		channel.send({ embeds: [embed] });
-	}
-}
 
 async function getAuditLog(message: Message<true>) {
 	const { guild, author, channel } = message;
@@ -72,4 +72,4 @@ async function getAuditLog(message: Message<true>) {
 	return log;
 }
 
-export default [selfDeleteLog];
+export default [deleteLog];
