393ead06aced583cde322dd141c0003797db09b3
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-10-20 18:00:22 +0900

    add: schema

---
+++ src/schemas/GuildCache.ts
@@ -0,0 +1,10 @@
+import { Schema, model } from 'mongoose';
+import { IServerSettings } from './ServerSettings';
+
+export interface IGuildCache {
+  serverId: string;
+}
+
+const GuildCache = new Schema<IGuildCache>({ serverId: { type: String, required: true, unique: true } });
+
+export default model<IServerSettings>('guild-cache', GuildCache);
