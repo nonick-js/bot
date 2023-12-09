b080aee84e4664e052d630d151036d4d8a617fba
akki256 <aki256@outlook.com>
2023-12-09 16:56:53 +0900

    add index.ts

---
+++ models/index.ts
@@ -0,0 +1,17 @@
+import AutomationSetting from "./automationSetting";
+import EventLogSetting from "./eventLogSetting";
+import GeneralSetting from "./generalSetting";
+import MessageSetting from "./messageSetting";
+import ModerateSetting from "./moderateSetting";
+import Notification from "./notification";
+import ServerData from "./serverData";
+
+export {
+  AutomationSetting,
+  EventLogSetting,
+  GeneralSetting,
+  MessageSetting,
+  ModerateSetting,
+  Notification,
+  ServerData,
+}
