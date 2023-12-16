109f36fbb289d27095be43283c5bcb7d33f89c08
akki256 <aki256@outlook.com>
2023-12-16 14:11:21 +0900

    fix lang default value

--- models/generalSetting.ts
+++ models/generalSetting.ts
@@ -1,10 +1,11 @@
+import { Locale } from 'discord.js';
 import { Schema, model } from 'mongoose';
 import { GeneralSettingSchema } from './types';
 import { LangKey, serverId } from './util';
 
 const schema = new Schema<GeneralSettingSchema>({
   serverId,
-  lang: { type: Schema.Types.String, enum: LangKey, default: 'ja-JP' },
+  lang: { type: Schema.Types.String, enum: LangKey, default: Locale.Japanese },
 });
 
 export default model<GeneralSettingSchema>('GeneralSetting', schema);
