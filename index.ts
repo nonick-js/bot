58ee22a249b820d2228063168b62fd0f126375c2
akki256 <aki256@outlook.com>
2024-03-14 16:44:09 +0900

    add test script

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.guild.create({
-    data: {
+  await prisma.guild
+    .create({
+      data: {
         guildId: '1234567890',
-    }
+      },
+    })
+    .catch(() => null);
+  await prisma.auditLog.create({
+    data: {
+      guildId: '1234567890',
+      author: '735110742222831657',
+      after: { name: 'NoNICK' },
+      before: { name: 'NULL' },
+    },
   });
-  console.log('success')
+  console.log('success');
+  const data = await prisma.guild.findMany({
+    where: { guildId: '1234567890' },
+    include: { auditLogs: true },
+  });
+  console.info(data);
 }
 
 main();
