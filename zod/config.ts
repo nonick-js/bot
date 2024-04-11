7d698b5ad468d34c6ff254f2aa18ae9a867d2d10
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-04-11 19:28:20 +0900

    prisma & zodスキーマの作成

---
+++ zod/config.ts
@@ -0,0 +1,147 @@
+import * as z from 'zod';
+import { MessageOption, Snowflake } from './discord';
+import { ChannelType, GuildVerificationLevel } from 'discord-api-types/v10';
+
+const hourError = { message: '0～23の間で設定する必要があります。' }
+
+const baseSchema = z.object({
+  guildId: Snowflake,
+});
+
+const LogConfig = z.discriminatedUnion('enabled', [
+  z.object({
+    enabled: z.literal(true),
+    channel: Snowflake,
+  }),
+  z.object({
+    enabled: z.literal(false),
+    channel: Snowflake.nullable(),
+  })
+]);
+
+// 入室メッセージ
+export const JoinMessageConfig = baseSchema.extend({
+  enabled:   z.boolean(),
+  channel:   Snowflake.nullable(),
+  message:   MessageOption,
+  ignoreBot: z.boolean(),
+}).superRefine((v, ctx) => {
+  if (v.enabled && !v.channel) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'チャンネルが設定されていません。',
+      path: ['channel'],
+    });
+  }
+});
+
+// 退室メッセージ
+export const LeaveMessageConfig = baseSchema.extend({
+  enabled:   z.boolean(),
+  channel:   Snowflake.nullable(),
+  message:   MessageOption,
+  ignoreBot: z.boolean(),
+}).superRefine((v, ctx) => {
+  if (v.enabled && !v.channel) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'チャンネルが設定されていません。',
+      path: ['channel'],
+    });
+  }
+});
+
+// サーバー内通報
+export const ReportConfig = baseSchema.extend({
+  channel:          Snowflake,
+  includeModerator: z.boolean(),
+  progressButton:   z.boolean(),
+  mentionEnabled:   z.boolean(),
+  mentionRole:      Snowflake.nullable(),
+}).superRefine((v, ctx) => {
+  if (v.mentionEnabled && !v.mentionRole) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'ロールが設定されていません。',
+      path: ['mentionRole'],
+    });
+  }
+});
+
+// イベントログ
+export const EventLogConfig = baseSchema.extend({
+  timeout:       LogConfig,
+  kick:          LogConfig,
+  ban:           LogConfig,
+  voice:         LogConfig,
+  messageDelete: LogConfig,
+  messageEdit:   LogConfig,
+})
+
+// メッセージURL展開
+export const MessageExpandConfig = baseSchema.extend({
+  enabled:            z.boolean(),
+  allowExternalGuild: z.boolean(),
+  ignoreTypes:        z.array(z.nativeEnum(ChannelType)),
+  ignoreChannels:     z.array(Snowflake),
+  ignorePrefixes:     z.array(z.string().length(1, 'プレフィックスは1文字である必要があります')).max(5, '5個以上のプレフィックスを登録することはできません。'),
+});
+
+// 自動アナウンス公開
+export const AutoPublicConfig = baseSchema.extend({
+  enabled:  z.boolean(),
+  channels: z.array(Snowflake),
+});
+
+// 自動スレッド作成
+export const AutoCreateThreadConfig = baseSchema.extend({
+  enabled:  z.boolean(),
+  channels: z.array(Snowflake),
+});
+
+// 自動認証レベル変更
+export const AutoChangeVerifyLevelConfig = baseSchema.extend({
+  enabled:    z.boolean(),
+  level:      z.nativeEnum(GuildVerificationLevel),
+  startHour:  z.coerce.number().int().min(0, hourError).max(23, hourError),
+  endHour:    z.coerce.number().int().min(0, hourError).max(23, hourError),
+  logEnabled: z.boolean(),
+  logChannel: Snowflake.nullable()
+}).superRefine((v, ctx) => {
+  if (v.startHour === v.endHour) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: '開始時刻と終了時刻を同じ値にすることはできません。',
+      path: ['starthour', 'endHour'],
+    });
+  }
+  if (v.enabled && v.logEnabled && !v.logChannel) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'チャンネルが設定されていません。',
+      path: ['logChannel'],
+    });
+  }
+});
+
+// AutoMod Plus
+export const AutoModConfig = baseSchema.extend({
+  enabled:          z.boolean(),
+  domainFilter:     z.boolean(),
+  domainFilterList: z.array(z.string().regex(/^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/, '無効なドメインです。')).max(20, '20個以上のドメインを登録することはできません。'),
+  tokenFilter:      z.boolean(),
+  inviteUrlFilter:  z.boolean(),
+  logEnabled:       z.boolean(),
+  logChannel:       Snowflake.nullable(),
+  ignoreChannels:   z.array(Snowflake),
+  ignoreRoles:      z.array(Snowflake),
+}).superRefine((v, ctx) => {
+  if (v.enabled && v.logEnabled && !v.logChannel) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'チャンネルが設定されていません。',
+      path: ['logChannel'],
+    });
+  }
+});
+
---
+++ zod/discord.ts
@@ -0,0 +1,85 @@
+import * as z from 'zod';
+
+export const Snowflake = z.string().regex(/^\d{17,19}$/, '無効なIDです。');
+
+export namespace Embed {
+  export const Thumbnail = z.object({
+    url:        z.string().url(),
+    proxy_url:  z.string().url().optional(),
+    height:     z.number().int().optional(),
+    width:      z.number().int().optional(),
+  });
+
+  export const Video = z.object({
+    url:       z.string().url().optional(),
+    proxy_url: z.string().url().optional(),
+    height:    z.number().int().optional(),
+    width:     z.number().int().optional(),
+  })
+  
+  export const Image = z.object({
+    url:       z.string().url(),
+    proxy_url: z.string().url().optional(),
+    height:    z.number().int().optional(),
+    width:     z.number().int().optional(),
+  });
+
+  export const Provider = z.object({
+    name: z.string().optional(),
+    url:  z.string().url().optional(),
+  })
+
+  export const Author = z.object({
+    name:           z.string().max(256, '256文字以下である必要があります。'),
+    url:            z.string().url().optional(),
+    icon_url:       z.string().url().optional(),
+    proxy_icon_url: z.string().url().optional(),
+  });
+
+  export const Footer = z.object({
+    text:           z.string().max(2048, '2048文字以下である必要があります。'),
+    icon_url:       z.string().url().optional(),
+    proxy_icon_url: z.string().url().optional(),
+  });
+
+  export const Field = z.object({
+    name:   z.string().max(256, '256文字以下である必要があります。'),
+    value:  z.string().max(1024, '1024文字以下である必要があります。'),
+    inline: z.boolean().optional(),
+  });
+
+  export const Structure = z.object({
+    title:       z.string().max(256, '256文字以下である必要があります。').optional(),
+    description: z.string().max(4096, '4096文字以下である必要があります。').optional(),
+    url:         z.string().url().optional(),
+    timestamp:   z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/, '無効な日付です。').optional(),
+    color:       z.number().int().optional(),
+    footer:      Footer.optional(),
+    image:       Image.optional(),
+    thumbnail:   Thumbnail.optional(),
+    video:       Video.optional(),
+    provider:    Provider.optional(),
+    author:      Author.optional(),
+    fields:      z.array(Field).max(25, 'フィールドは25個以下である必要があります。').optional(),
+  }).superRefine((v, ctx) => {
+    if ([
+      v.title?.length,
+      v.description?.length,
+      v.fields?.reduce((sum, str) => sum + str.name.length, 0),
+      v.fields?.reduce((sum, str) => sum + str.value.length, 0),
+      v.author?.name.length,
+    ].map(v => v || 0).reduce((sum, num) => sum + num, 0) > 6000) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '埋め込みの文字数が合計で6000文字を超えています。',
+        path: ['description'],
+      });
+    }
+  });
+}
+
+export const MessageOption = z.object({
+  content: z.string().max(2000, '2000文字以下である必要があります。').optional(),
+  embeds:  z.array(Embed.Structure).max(10, '埋め込みは10個以下である必要があります。').optional(),
+  // 必要に応じて他のプロパティを追加
+});
