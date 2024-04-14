046fd8a2b3d84b17e903c5b07226c9676e3e7cc8
akki256 <aki256@outlook.com>
2024-04-14 19:04:49 +0900

    fix getMessage

--- src/modules/util.ts
+++ src/modules/util.ts
@@ -8,18 +8,20 @@ export async function getMessage(
   ...id: [channelId: string, messageId: string]
 ): Promise<Message>;
 export async function getMessage(...id: string[]): Promise<Message> {
-  const guild =
-    id.length > 2 ? await client.guilds.fetch(id[0]).catch(() => null) : client;
-  if (!guild) throw new URIError(`サーバーID:\`${id[0]}\`に入っていません`);
-  const channel = await guild.channels.fetch(id[1]).catch(() => null);
+  const [messageId, channelId, guildId = null] = id.slice(0, 3).reverse();
+  const guild = guildId
+    ? await client.guilds.fetch(guildId).catch(() => null)
+    : client;
+  if (!guild) throw new URIError(`サーバーID:\`${guildId}\`に入っていません`);
+  const channel = await guild.channels.fetch(channelId).catch(() => null);
   if (!channel?.isTextBased())
     throw new URIError(
-      `チャンネルID:\`${id[1]}\`が存在しないもしくはアクセスできません`,
+      `チャンネルID:\`${channelId}\`が存在しないもしくはアクセスできません`,
     );
-  const message = await channel.messages.fetch(id[2]).catch(() => {});
+  const message = await channel.messages.fetch(messageId).catch(() => {});
   if (!message)
     throw new URIError(
-      `メッセージID:\`${id[2]}\`が存在しないもしくはアクセスできません`,
+      `メッセージID:\`${messageId}\`が存在しないもしくはアクセスできません`,
     );
   return message;
 }
