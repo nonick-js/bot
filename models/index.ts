49f6428de8019fabdc9366fef5cf18d420a72216
akki256 <aki256@outlook.com>
2024-04-15 20:38:19 +0900

    add index.ts

---
+++ models/index.ts
@@ -0,0 +1,24 @@
+import AuditLogConfig from './auditLog';
+import AutoChangeVerifyLevelConfig from './autoChangeVerifyLevelConfig';
+import AutoCreateThreadConfig from './autoCreateThreadConfig';
+import AutoModConfig from './autoModConfig';
+import AutoPublicConfig from './autoPublicConfig';
+import EventLogConfig from './eventLogConfig';
+import JoinMessageConfig from './joinMessageConfig';
+import LeaveMessageConfig from './leaveMessageConfig';
+import MessageExpandConfig from './messageExpandConfig';
+import ReportConfig from './reportConfig';
+export * as Utils from './util';
+
+export {
+  AuditLogConfig,
+  AutoChangeVerifyLevelConfig,
+  AutoCreateThreadConfig,
+  AutoModConfig,
+  AutoPublicConfig,
+  EventLogConfig,
+  JoinMessageConfig,
+  LeaveMessageConfig,
+  MessageExpandConfig,
+  ReportConfig,
+};
