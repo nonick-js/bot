6f03fef38f8702a5a704fc90e451bb2c231472d7
akki256 <aki256@outlook.com>
2024-04-15 20:44:39 +0900

    add joinMessage

---
+++ src/events/message/join.ts
@@ -0,0 +1,26 @@
+import { joinAndLeaveHolder } from '@const/holder';
+import { JoinMessageConfig } from '@models';
+import { DiscordEventBuilder } from '@modules/events';
+import { Events } from 'discord.js';
+
+export default new DiscordEventBuilder({
+  type: Events.GuildMemberAdd,
+  async execute(member) {
+    const setting = await JoinMessageConfig.findOne({
+      guildId: member.guild.id,
+    });
+    if (!setting?.enabled) return;
+    if (setting.ignoreBot && member.user.bot) return;
+    const channel = setting.channel
+      ? await member.guild.channels.fetch(setting.channel).catch(() => null)
+      : null;
+    if (channel?.isTextBased()) {
+      channel.send(
+        joinAndLeaveHolder.parse(setting.message, {
+          guild: member.guild,
+          user: member.user,
+        }),
+      );
+    }
+  },
+});
