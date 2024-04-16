7b45430eee7beda1dc34afe8d0059d951eb59165
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-04-16 09:49:00 +0900

    index.tsにguildスキーマを追加

--- models/index.ts
+++ models/index.ts
@@ -1,9 +1,10 @@
-import AuditLogConfig from './auditLog';
+import AuditLog from './auditLog';
 import AutoChangeVerifyLevelConfig from './autoChangeVerifyLevelConfig';
 import AutoCreateThreadConfig from './autoCreateThreadConfig';
 import AutoModConfig from './autoModConfig';
 import AutoPublicConfig from './autoPublicConfig';
 import EventLogConfig from './eventLogConfig';
+import Guild from './guild';
 import JoinMessageConfig from './joinMessageConfig';
 import LeaveMessageConfig from './leaveMessageConfig';
 import MessageExpandConfig from './messageExpandConfig';
@@ -11,12 +12,13 @@ import ReportConfig from './reportConfig';
 export * as Utils from './util';
 
 export {
-  AuditLogConfig,
+  AuditLog,
   AutoChangeVerifyLevelConfig,
   AutoCreateThreadConfig,
   AutoModConfig,
   AutoPublicConfig,
   EventLogConfig,
+  Guild,
   JoinMessageConfig,
   LeaveMessageConfig,
   MessageExpandConfig,
