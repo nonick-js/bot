ce862a78704bca51e759405eda7c1f106f6fc9e7
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-04-18 01:08:57 +0900

    auditLogのguildIdがunique属性になっていた問題を修正

--- models/auditLog.ts
+++ models/auditLog.ts
@@ -1,9 +1,11 @@
 import { type Model, Schema, model, models } from 'mongoose';
 import type { AuditLog } from '../zod/auditLog';
-import { guildId } from './util';
 
 const AuditLogSchema = new Schema<typeof AuditLog._type>({
-  guildId,
+  guildId: {
+    required: true,
+    type: Schema.Types.String,
+  },
   after: Schema.Types.Mixed,
   authorId: Schema.Types.String,
   before: Schema.Types.Mixed,
