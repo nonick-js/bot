0f96a338dbb4cf2a4344f58b271d32d42a48f745
nonick <87268971+nonick-mc@users.noreply.github.com>
2024-10-16 01:48:01 +0900

    feat: AutoMod Plusのドメイン設定を文字列から配列に変換するように変更

--- zod/autoModConfig.ts
+++ zod/autoModConfig.ts
@@ -1,19 +1,25 @@
 import { z } from 'zod';
 import { Snowflake } from './util';
 
+const domainRegex = /^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/;
+
 const AutoModConfig = z
   .object({
     enabled: z.boolean(),
     filter: z.object({
       domain: z.object({
         enabled: z.boolean(),
-        list: z
-          .array(
-            z
-              .string()
-              .regex(/^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/, '無効なドメインです'),
-          )
-          .max(20, '20個以上のドメインを登録することはできません'),
+        list: z.preprocess(
+          (v) =>
+            String(v)
+              .split(/,|\n/)
+              .reduce<string[]>((acc, item) => {
+                const trimmed = item.trim();
+                if (trimmed) acc.push(trimmed);
+                return acc;
+              }, []),
+          z.array(z.string()).max(20, '20個以上のドメインを登録することはできません'),
+        ),
       }),
       token: z.boolean(),
       inviteUrl: z.boolean(),
@@ -28,6 +34,14 @@ const AutoModConfig = z
     }),
   })
   .superRefine((v, ctx) => {
+    if (!v.filter.domain.list.every((v) => domainRegex.test(v))) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '無効なドメインが含まれています',
+        path: ['filter.domain.list'],
+      });
+    }
+
     if (v.enabled && v.log.enabled && !v.log.channel) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
