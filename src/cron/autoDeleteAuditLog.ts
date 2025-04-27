a987e6b3d1ca644574042a6db90159d89c57c05a
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-04-27 16:02:28 +0000

    feat: 監査ログの自動削除機能を追加

---
+++ src/cron/autoDeleteAuditLog.ts
@@ -0,0 +1,11 @@
+﻿import { auditLog } from '@database/src/schema/audit-log';
+import { CronBuilder } from '@modules/cron';
+import { db } from '@modules/drizzle';
+import { lt } from 'drizzle-orm';
+
+export default new CronBuilder({ minute: 0, hour: 0 }, async () => {
+  const thirtyDaysAgo = new Date();
+  thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+  await db.delete(auditLog).where(lt(auditLog.createdAt, thirtyDaysAgo));
+});
