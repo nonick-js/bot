2d8397eb6abcf5b3f41bdc24c1a6fc07511c22fa
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-05-12 04:55:17 +0000

    feat: 30日が経過したデータを自動で削除するように変更

--- src/cron/autoDeleteDatabase.ts
+++ src/cron/autoDeleteDatabase.ts
@@ -1,4 +1,5 @@
 ﻿import { auditLog } from '@database/src/schema/audit-log';
+import { report } from '@database/src/schema/report';
 import { CronBuilder } from '@modules/cron';
 import { db } from '@modules/drizzle';
 import { lt } from 'drizzle-orm';
@@ -8,4 +9,5 @@ export default new CronBuilder({ minute: 0, hour: 0 }, async () => {
   thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
   await db.delete(auditLog).where(lt(auditLog.createdAt, thirtyDaysAgo));
+  await db.delete(report).where(lt(report.createdAt, thirtyDaysAgo));
 });
