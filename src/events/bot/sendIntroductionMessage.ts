05756c60ca6fb2791b63f0212fb5224f5abb101c
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-05-16 05:27:50 +0000

    feat: BOTの導入者を検知するeventを作成

---
+++ src/events/bot/sendIntroductionMessage.ts
@@ -0,0 +1,22 @@
+﻿import { DiscordEventBuilder } from '@modules/events';
+import { AuditLogEvent, Events } from 'discord.js';
+
+export default new DiscordEventBuilder({
+  type: Events.GuildCreate,
+  async execute(guild) {
+    const auditLogs = await guild
+      .fetchAuditLogs({
+        limit: 1,
+        type: AuditLogEvent.BotAdd,
+      })
+      .catch(() => null);
+    if (!auditLogs) return;
+
+    const entry = auditLogs.entries.first();
+    if (!entry || entry.targetId !== guild.client.user.id) return;
+
+    entry.executor?.send({
+      content: 'BOTの導入を検知しました',
+    });
+  },
+});
