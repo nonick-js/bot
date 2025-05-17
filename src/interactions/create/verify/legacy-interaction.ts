1ba3b9d7cc2629dd0e11082e7af7134f7f672744
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2025-05-18 00:45:14 +0900

    feat: /create verify-panelの作成 (#155)
    * feat: /create verify-panelの作成
    
    * feat: ボタンインタラクションを追加、既存のインタラクションとの統一

---
+++ src/interactions/create/verify/legacy-interaction.ts
@@ -0,0 +1,42 @@
+import { Button } from '@akki256/discord-interaction';
+import { GuildMemberRoleManager, inlineCode } from 'discord.js';
+import {
+  verifyForButtonCaptcha,
+  verifyForImageCaptcha,
+} from 'interactions/create/verify/_function';
+
+const verifyButton = new Button(
+  {
+    customId: /^nonick-js:verify-(button|image)$/,
+  },
+  async (interaction) => {
+    if (!interaction.inCachedGuild()) return;
+
+    const roleId =
+      interaction.message.embeds[0]?.fields[0]?.value?.match(
+        /(?<=<@&)\d+(?=>)/,
+      )?.[0];
+    const roles = interaction.member.roles;
+
+    if (!roleId || !(roles instanceof GuildMemberRoleManager))
+      return interaction.reply({
+        content: `${inlineCode('❌')} 認証中に問題が発生しました。`,
+        ephemeral: true,
+      });
+    if (roles.cache.has(roleId))
+      return interaction.reply({
+        content: `${inlineCode('✅')} 既に認証されています。`,
+        ephemeral: true,
+      });
+
+    if (interaction.customId === 'nonick-js:verify-button') {
+      return verifyForButtonCaptcha(interaction, roleId);
+    }
+
+    if (interaction.customId === 'nonick-js:verify-image') {
+      return verifyForImageCaptcha(interaction, roleId);
+    }
+  },
+);
+
+export default [verifyButton];
